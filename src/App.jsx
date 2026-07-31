b522cb6cd5756c0f84f93f8ac9707da5fe68ddba
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-07-31 19:23:38 +0200

    Implement init home view

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Home from "./components/Home";
+import PageTitle from "./components/heading/PageTitle";
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   );
