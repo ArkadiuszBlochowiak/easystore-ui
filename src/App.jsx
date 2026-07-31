abc583d589a067493aa9b7f9766f420a81a6bab8
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-07-31 19:10:36 +0200

    move header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Footer from "./components/footer/Footer";
-import Header from "./components/Header";
+import Header from "./components/header/Header";
 
 function App() {
   return (
