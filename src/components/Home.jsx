2347665d74ebe8d40bc70458f428e3a9b61ce977
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-02 21:19:00 +0200

    config axios get endpoint

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,21 @@
 import PageHeading from "./heading/PageHeading";
 import ProductListing from "./product/ProductListing";
-import { products } from "../data/product";
+// import { products } from "../data/product";
+import apiClients from "../api/apiClient";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const response = await apiClients.get("/products");
+    setProducts(response.data);
+  };
+
   return (
     <div className="home-container">
       <PageHeading title="Explore Eazy Stickers!">
