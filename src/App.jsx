c1789070cb390edbfb7f0dcdeb0878c486b50fb6
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-03 16:36:43 +0200

    lint fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/Home";
-import PageTitle from "./components/heading/PageTitle";
 
 function App() {
   return (
