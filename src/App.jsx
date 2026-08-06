982ed6edd4dc7bd4fca0eb457345150ff386eb3f
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-06 11:11:38 +0200

    implement tailwind styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/Home";
 
 function App() {
   return (
-    <>
+    <div className="bg-gray-100">
       <Header />
       <Home />
       <Footer />
-    </>
+    </div>
   );
 }
 
