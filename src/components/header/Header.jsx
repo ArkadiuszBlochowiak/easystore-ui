df39492eb0381eec0a828776d626ece8cfd44445
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-07 15:44:35 +0200

    show active link

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,6 +12,9 @@ export default function Header() {
   const navLinkClass =
     "text-center text-lg font-primary font-semibold text-primary py-2 dark:text-light hover:text-dark dark:hover:text-lighter";
 
+  const activeLinkClass = ({ isActive }) =>
+    isActive ? `underline ${navLinkClass}` : navLinkClass;
+
   return (
     <header className="border-b border-gray-300 dark:border-gray-600 sticky top-0 z-20 bg-normalbg dark:bg-darkbg">
       <div className="flex items-center justify-between mx-auto max-w-6xl px-6 py-4">
@@ -23,22 +26,22 @@ export default function Header() {
           <DarkModeSwitch />
           <ul className="flex space-x-6">
             <li>
-              <NavLink to="/" className={navLinkClass}>
+              <NavLink to="/" className={activeLinkClass}>
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink to="/about" className={navLinkClass}>
+              <NavLink to="/about" className={activeLinkClass}>
                 About
               </NavLink>
             </li>
             <li>
-              <NavLink to="/contact" className={navLinkClass}>
+              <NavLink to="/contact" className={activeLinkClass}>
                 Contact
               </NavLink>
             </li>
             <li>
-              <NavLink to="/login" className={navLinkClass}>
+              <NavLink to="/login" className={activeLinkClass}>
                 Login
               </NavLink>
             </li>
