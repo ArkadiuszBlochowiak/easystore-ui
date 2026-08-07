8f2c2a96327faa6b1205372673918e6d0ec608a0
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-07 11:55:54 +0200

    move theme switch to header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingBasket, faTags } from "@fortawesome/free-solid-svg-icons";
-import DarkModeSwitch from "./DarkModeSwitch";
+import {
+  faShoppingBasket,
+  faTags,
+  faMoon,
+  faSun,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export default function Header() {
   const navLinkClass =
@@ -45,5 +50,35 @@ export default function Header() {
         </nav>
       </div>
     </header>
+  );
+}
+
+function DarkModeSwitch() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    const newValue = theme === "dark" ? "light" : "dark";
+    setTheme(newValue);
+
+    if (newValue === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  return (
+    <div className="flex items-center py-2 z-10">
+      <button
+        className="flex items-center justify-center mx-3 w-8 h-8 rounded-full border border-primary dark:border-light transition duration-300 hover:bg-gray-300 dark:hover:bg-gray-600"
+        aria-label="Toggle theme"
+        onClick={toggleTheme}
+      >
+        <FontAwesomeIcon
+          icon={theme === "dark" ? faMoon : faSun}
+          className="w-4 h-4 dark:text-light text-primary"
+        />
+      </button>
+    </div>
   );
 }
