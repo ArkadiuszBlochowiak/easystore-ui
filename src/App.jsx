9158b3205a2e9cc571337921e28e433a3d73763f
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-12 14:02:38 +0200

    initialize cart context

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,34 @@ import Header from "./components/header/Header";
 import { useNavigation } from "react-router-dom";
 import { ToastContainer, Bounce } from "react-toastify";
 import { isDarkThemeVisible } from "./utils/utils";
+import { CartContext } from "./store/cart-context";
 
 function App() {
   const navigation = useNavigation();
   const isDarkTheme = isDarkThemeVisible();
+  const initialCartContext = {
+    cart: [],
+    setCart: () => {},
+    addToCart: () => {},
+    removeFromCart: () => {},
+    totalQuantity: 0,
+  };
 
   return (
     <div className="bg-normalbg dark:bg-darkbg min-h-screen flex flex-col">
-      <Header />
-      {navigation.state === "loading" ? (
-        <div className="flex grow items-center justify-center">
-          <span className="text-2xl font-semibold text-primary dark:text-light">
-            Loading...
-          </span>
-        </div>
-      ) : (
-        <Outlet />
-      )}
-
-      <Footer />
+      <CartContext value={initialCartContext}>
+        <Header />
+        {navigation.state === "loading" ? (
+          <div className="flex grow items-center justify-center">
+            <span className="text-2xl font-semibold text-primary dark:text-light">
+              Loading...
+            </span>
+          </div>
+        ) : (
+          <Outlet />
+        )}
+        <Footer />
+      </CartContext>
       <ToastContainer
         position="top-center"
         autoClose={3000}
